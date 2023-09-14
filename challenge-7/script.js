6a338bf128f937c5f08511a39b67eccbe3ed991c
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-14 10:42:15 +0200

    Refactoring

--- challenge-7/script.js
+++ challenge-7/script.js
@@ -3,23 +3,12 @@ const texts = document.querySelectorAll(".faq-section-description");
 const container = document.querySelector(".faq-container");
 
 const toogle = (e) => {
-  const targetClass = e.target.classList.value.includes("arrow");
-  const targetToToggle = e.target.classList[1];
-  const arrowToChange = e.target;
-  // const arrowToChange = Array.from(arrows)
-  //   .map((arrow) => arrow.classList)
-  //   .filter((arrow) => arrow === targetToToggle);
-  if (targetClass) {
-    arrowToChange.src = arrowToChange.src = "./assets/arrow-right.svg" ? "./assets/arrow-down.svg" : "./assets/arrow-right.svg";
-
-    texts.forEach((text) => {
-      if (Array.from(text.classList).some((str) => str === targetToToggle) && text.style.display !== "block") {
-        text.style.display = "block";
-      } else {
-        text.style.display = "none";
-      }
-    });
-  }
+  const arrowToChange = e.target.closest(".arrow-img");
+  const targetText = document.querySelector(`.${arrowToChange.dataset.target}`);
+  if (!arrowToChange) return;
+  if (!targetText) return;
+  arrowToChange.classList.toggle("arrow-rotate");
+  targetText.classList.toggle("show-text");
 };
 
 container.addEventListener("click", toogle);
